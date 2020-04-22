47011b00aa72a6ca7c2f705941afd323dc0af7ed
cyberspacedk <frontend.dk@gmail.com>
2020-04-22 14:05:32 +0300

    move error boundary outside

---
+++ src/components/ErrorBoundary.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export default class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static defaultProps = {
+    fallback: 'Something went wrong !'
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // You can also log the error to an error reporting service
+    console.log(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+    return <h1>{this.props.fallback}</h1>;
+    }
+
+    return this.props.children; 
+  }
+}
--- src/lessons/101/app.js
+++ src/lessons/101/app.js
@@ -1,16 +1,24 @@
 import React from "react";
+import ErrorBoundary from '../../components/ErrorBoundary';
+
 // 1. Change this static import to a dynamic import, wrapped in React.lazy
 // import PokemonDetail from "./pokemon-detail";
 
-const PokemonDetail = React.lazy(()=> import('./pokemon-detail'))
+const PokemonDetail = React.lazy(()=> import("./pokemon-detail"))
 
 export default function App() {
   return (
     <div>
+      <h1>Pokedex</h1>
+
+      {/* Wrap area that could down app */}
+      <ErrorBoundary fallback="Couldn`t catch">
       {/* 2. Wrap this component in a React.Suspense component with fallback */}
-      <React.Suspense fallback="Loading. Wait ...."> 
-        <PokemonDetail />
-      </React.Suspense>
+        <React.Suspense fallback="Loading. Wait ...."> 
+          <PokemonDetail />
+        </React.Suspense>
+      </ErrorBoundary>
     </div>
+   
   );
-}
+} 
